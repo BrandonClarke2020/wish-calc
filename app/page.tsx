1af118fcca605371c994d62e45cdeb3e43730ae5
BrandonClarke2020 <BrandonClarke2020@proton.me>
2025-10-06 17:51:00 -0400

    Update dependencies, removed sass, depricated advanced, fixed typos

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 const helloWorld = () => {
-  
   return (
-    <div className="title">
+    <header className="title">
       <h1>Genshin Wish Theory</h1>
-    </div>
+      <h2>Page under construction. Soon to be a full portfolio site.</h2>
+    </header>
   );
-}
+};
 
-export default helloWorld
+export default helloWorld;
